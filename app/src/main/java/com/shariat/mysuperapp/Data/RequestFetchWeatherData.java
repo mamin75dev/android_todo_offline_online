10f6767ada59f8ac35cf7654df3966bfe393c907
mohamadamin <mamin75.dev@gmail.com>
2021-10-21 13:18:55 +0330

    complete weather api

--- app/src/main/java/com/shariat/mysuperapp/Data/RequestFetchWeatherData.java
+++ app/src/main/java/com/shariat/mysuperapp/Data/RequestFetchWeatherData.java
@@ -12,7 +12,7 @@ import java.util.Locale;
 public class RequestFetchWeatherData extends JsonObjectRequest {
 
   public static final String APP_KEY = "734adc24ded4d6f5b88e0ec4c23dba8b";
-  public static final String URL_FORMAT = "http://api.openweathermap.org/data/2.5/weather?q=%s&appid=" + APP_KEY;
+  public static final String URL_FORMAT = "http://api.openweathermap.org/data/2.5/weather?q=%s&units=metric&appid=" + APP_KEY;
 
   public RequestFetchWeatherData(
       int method,
--- app/src/main/java/com/shariat/mysuperapp/UI/WeatherFragment.java
+++ app/src/main/java/com/shariat/mysuperapp/UI/WeatherFragment.java
@@ -6,6 +6,8 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -20,11 +22,14 @@ import com.android.volley.toolbox.Volley;
 import com.shariat.mysuperapp.Data.RequestFetchWeatherData;
 import com.shariat.mysuperapp.R;
 
+import org.json.JSONException;
 import org.json.JSONObject;
 
 public class WeatherFragment extends Fragment {
 
-  TextView cityTV, weatherTV, degreeTV, humidityTV, timeTV;
+  TextView cityTV, weatherTV, degreeTV, humidityTV, timeTV, descTV, windSpeedTV;
+  ImageView windImage, humidityImage;
+  ProgressBar progressBar;
 
   private final String LOG_TAG = this.getClass().getSimpleName();
 
@@ -38,7 +43,12 @@ public class WeatherFragment extends Fragment {
     weatherTV = view.findViewById(R.id.weather_tv);
     degreeTV = view.findViewById(R.id.degree_tv);
     humidityTV = view.findViewById(R.id.humidity_tv);
-    timeTV = view.findViewById(R.id.time_tv);
+//    timeTV = view.findViewById(R.id.time_tv);
+    descTV = view.findViewById(R.id.desc_tv);
+    windSpeedTV = view.findViewById(R.id.wind_speed_tv);
+    windImage = view.findViewById(R.id.wind_img);
+    humidityImage = view.findViewById(R.id.humidity_img);
+    progressBar = view.findViewById(R.id.progress_bar);
     queue = Volley.newRequestQueue(getContext());
 //    setData();
     requestData("Tehran");
@@ -57,7 +67,25 @@ public class WeatherFragment extends Fragment {
         new Response.Listener<JSONObject>() {
           @Override
           public void onResponse(JSONObject response) {
-            Log.i(LOG_TAG, "response: \n" + response);
+            try {
+              windImage.setVisibility(View.VISIBLE);
+              humidityImage.setVisibility(View.VISIBLE);
+              progressBar.setVisibility(View.GONE);
+              cityTV.setText(response.getString("name") + ", " + response.getJSONObject("sys").getString("country"));
+              descTV.setText(response.getJSONArray("weather").getJSONObject(0).getString("main"));
+              weatherTV.setText(
+                  getWeatherConditionBaseOnWeatherID(
+                      response.getJSONArray("weather").getJSONObject(0).getInt("id")
+                  )
+              );
+              humidityTV.setText(response.getJSONObject("main").getInt("humidity") + "%");
+              windSpeedTV.setText(response.getJSONObject("wind").getInt("speed") + " m/s");
+              degreeTV.setText(
+                  response.getJSONObject("main").getInt("temp") + " " + Html.fromHtml("&#8451;")
+              );
+            } catch (JSONException e) {
+              e.printStackTrace();
+            }
           }
         },
         new Response.ErrorListener() {
@@ -68,5 +96,26 @@ public class WeatherFragment extends Fragment {
         }
     );
     queue.add(request);
+  }
+
+  private String getWeatherConditionBaseOnWeatherID(int weatherId) {
+    if (weatherId == 800) {
+      return getResources().getString(R.string.wi_forecast_io_clear_day);
+    }
+    switch ((int) Math.floor(weatherId / 100)) {
+      case 2:
+        return getResources().getString(R.string.wi_day_thunderstorm);
+      case 3:
+      case 5:
+        return getResources().getString(R.string.wi_day_rain);
+      case 6:
+        return getResources().getString(R.string.wi_day_snow);
+      case 7:
+        return getResources().getString(R.string.wi_forecast_io_clear_day);
+      case 8:
+        return getResources().getString(R.string.wi_day_cloudy);
+      default:
+        return "";
+    }
   }
 }
